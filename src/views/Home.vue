f311bff6a16fa81d22b848e83f697a3dd785ebd5
Mychen3 <97491530@qq.com>
2020-12-22 18:08:46 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+ <div class="content">
+      <div class="content-div">
+         123
+      </div>
+ </div>
+</template>
+
+<script>
+
+
+    export default {
+        components: {
+
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+
+
+    .content{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+    .content-div{
+        background-color:#42b983;
+        width: 200px;
+        height: 200px;
+    }
+
+
+</style>
+
